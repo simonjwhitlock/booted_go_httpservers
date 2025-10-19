6d20a129133f0cca8513f6a2492a9d45c0295cae
Simon Whitlock <simonjwhitlock@gmail.com>
2025-10-19 12:52:53 +0100

    routing starter

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// ...
+}
 
 func main() {
 	mux := http.NewServeMux()
+	// Serve the logo.png file at the /assets path
+	// Serve files from the current directory under the /app/ path, stripping the /app/ prefix
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	// Serve the logo.png file at the /assets path
+	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	Server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-
 	Server.ListenAndServe()
 
 }
