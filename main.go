34c5a231e1da73a2ec883b9f0ac9d37636a50660
simon whitlock <simonjwhitlock@gmail.com>
2025-10-19 14:13:10 +0100

    stateful-testing1

--- main.go
+++ main.go
@@ -2,45 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"sync/atomic"
 )
 
 type apiConfig struct {
-	fileserverHits atomic.Int32
+	fileserverHits int
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileserverHits.Add(1)
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	var apiCfg apiConfig
-	apiCfg.fileserverHits.Store(0)
+
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
 	// Serve the logo.png file at the /assets path
 	// Serve files from the current directory under the /app/ path, stripping the /app/ prefix
-	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	handler := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(handler)))
 	// Serve the logo.png file at the /assets path
-	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-	mux.HandleFunc("/metrics/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		str := fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load())
-		w.Write([]byte(str))
-	})
+	mux.Handle("/assets/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/healthz/", http.HandlerFunc(handlerReadiness))
+	mux.Handle("/metrics/", http.HandlerFunc(apiCfg.handlerMetrics))
+	mux.Handle("/reset/", http.HandlerFunc(apiCfg.handlerResetMetrics))
 
-	Server := http.Server{
-		Addr:    ":8080",
+	Server := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	Server.ListenAndServe()
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(Server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (c *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	str := fmt.Sprintf("Hits: %v\n", c.fileserverHits)
+	w.Write([]byte(str))
+}
+
+func (c *apiConfig) handlerResetMetrics(w http.ResponseWriter, req *http.Request) {
+	c.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
