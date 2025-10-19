ce3b73854c7e11cebcd020a28c7186dcaa117888
simon whitlock <simonjwhitlock@gmail.com>
2025-10-19 13:43:52 +0100

    stateful-tesing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync/atomic"
 )
@@ -10,20 +11,30 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	// ...
+	cfg.fileserverHits.Add(1)
+	return next
 }
 
 func main() {
+	var apiCfg apiConfig
+	apiCfg.fileserverHits.Store(0)
 	mux := http.NewServeMux()
 	// Serve the logo.png file at the /assets path
 	// Serve files from the current directory under the /app/ path, stripping the /app/ prefix
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	// Serve the logo.png file at the /assets path
 	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		str := fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load())
+		w.Write([]byte(str))
 	})
 
 	Server := http.Server{
